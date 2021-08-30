6e5db97c9dd0afabbe729bf2de198e6bf2d8b3bd
Almer ibarra <61256492+Sorvereign@users.noreply.github.com>
2021-08-30 11:14:37 -0500

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,12 @@
+AOS.init({
+  duration: 500,
+})
+
+// Grab HTML Elements
+	const btn = document.querySelector("button.mobile-menu-button");
+	const menu = document.querySelector(".mobile-menu");
+
+	// Add Event Listeners
+	btn.addEventListener("click", () => {
+	menu.classList.toggle("hidden");
+	});
